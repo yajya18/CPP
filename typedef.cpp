f5a4fb58225bf97b4de6291c6ad07486a1e88d6d
yajya18 <yajyaarora18@gmail.com>
2024-11-20 15:01:52 +0530

    edit

--- typedef.cpp
+++ typedef.cpp
@@ -6,8 +6,12 @@ using str_t = std::string;
 
 int main(){
     using namespace std;
-    std::string FirstName = "Yajya";
-    str_t LastName = "Arora";
-    cout<<FirstName<<" "<<LastName;
+    std::string FirstName;
+    str_t LastName;
+    cout<<"What is your first name?";
+    cin>>FirstName;
+    cout<<'\n'<<"What is your Last name";
+    cin>>LastName;
+    cout<<'\n'<<"Hi, "<<FirstName<<" "<<LastName;
 return 0;
 }
